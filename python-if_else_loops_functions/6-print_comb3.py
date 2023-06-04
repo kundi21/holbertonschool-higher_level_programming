49d575d6b846871b604ab43c0d90e14b52848378
kundi21 <cundi.machado@gmail.com>
2023-06-04 06:48:44 -0700

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(10):
     for j in range(i, 10):
         if i != j:
-            print(f"{i}{j}", end="")
+            print("{}{}".format(i, j), end="")
             if i != 8:
                 print(", ", end="")
 print()
