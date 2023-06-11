b0c6dd97b35da09ece121a6aee70b63b5b362efd
kundi21 <cundi.machado@gmail.com>
2023-06-11 11:33:59 -0700

    8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
     length = len(sentence)
     first_char = None
     if sentence is None or sentence == "":
-     return(length, first_char)
+        return (length, first_char)
     else:
-       first_char = sentence[0]
-       return(length, first_char)
+        first_char = sentence[0]
+        return (length, first_char)
