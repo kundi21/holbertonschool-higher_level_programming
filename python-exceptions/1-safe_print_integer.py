18e23350f35e0ba538ebfd5a5f73959d729236c5
kundi21 <cundi.machado@gmail.com>
2023-06-11 18:24:20 -0700

    1-safe_print_integer.py

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return True
+    except ValueError:
+        pass
+    else:
+        return False
