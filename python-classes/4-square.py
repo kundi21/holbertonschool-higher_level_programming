beb50ac30d83226980f0ca9e7198b3b8801b2991
kundi21 <cundi.machado@gmail.com>
2023-06-14 15:50:31 -0700

    4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Squirtle"""
+
+
+class Square:
+    """Squirtle Squirtle"""
+
+    def __init__(self, size=0):
+        """Size"""
+        self.__size = size
+
+    @property
+    def size(self):
+        "Getter"
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        "Setter"
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Areaaaaaa"""
+        self.area = self.__size * self.__size
+        return self.area
+
+
+pass
