6de8c7d667e3c990e1bee627d621b3efb2b48ec2
kundi21 <cundi.machado@gmail.com>
2023-06-14 09:34:33 -0700

    3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Squirtle"""
+
+
+class Square:
+    """Squirtle Squirtle"""
+
+    def __init__(self, size=0):
+        """Size"""
+        self.__size = size
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+        def area(self):
+            """Area, penal juez"""
+            self.area = area
+            return area
+        pass
