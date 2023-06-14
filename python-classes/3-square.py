2b60e6899a7cd3724ff242acdb5cd5a706fc9e64
kundi21 <cundi.machado@gmail.com>
2023-06-14 10:03:02 -0700

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -8,14 +8,15 @@ class Square:
     def __init__(self, size=0):
         """Size"""
         self.__size = size
+
         if isinstance(size, int):
             if size < 0:
                 raise ValueError("size must be >= 0")
         else:
             raise TypeError("size must be an integer")
 
-        def area(self):
-            """Area, penal juez"""
-            self.area = area
-            return area
-        pass
+    def area(self):
+        """Areaaaaaa"""
+        self.area = self.__size * self.__size
+        return self.area
+    pass
