bd4fe8eaba7f6a8eedd7ed12174b1f26d53676e0
kundi21 <cundi.machado@gmail.com>
2023-06-14 09:29:08 -0700

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size=0):
         """Size"""
         self.__size = size
-        if isinstance(size):
+        if isinstance(size, int):
             if size < 0:
                 raise ValueError("size must be >= 0")
         else:
