0c27d7a88fe7cb6ce9cd4a6af2d90589b47b83bb
kundi21 <cundi.machado@gmail.com>
2023-06-15 11:02:19 -0700

    5-square.py

---
+++ python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Squirtle"""
+
+
+class Square:
+    """Squirtle Squirtle"""
+
+    def __init__(self, size=0):
+        """Size"""
+        self.__size = size
+
+    @property
+    def size(self):
+        "Getter"
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        "Setter"
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Areaaaaaa"""
+        self.area = self.__size * self.__size
+        return self.area
+
+    def my_print(self):
+        "My_print"
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for x in range(self.__size):
+                print("#", end="")
+            print()
