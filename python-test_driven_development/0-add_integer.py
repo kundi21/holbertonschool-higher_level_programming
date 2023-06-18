1c0f6ce2794ed7305235e9a850235557aa26b05c
kundi21 <cundi.machado@gmail.com>
2023-06-18 16:15:30 -0700

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,16 +2,10 @@
 
 def add_integer(a, b=98):
     """
-    Test add_integer
-
-    >>> add_integer(1, 98)
-    99
-
+    Add two integers
     """
-    a = 1
-    return a + b
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+    if not isinstance(a, (int, float)):
+        raise TypeError('a must be an integer')
+    if not isinstance(b, (int, float)):
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
