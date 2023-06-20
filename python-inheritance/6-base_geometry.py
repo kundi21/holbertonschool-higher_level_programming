b956797ac5d3ee424974dc4dfde1e6e5aaf843fd
kundi21 <cundi.machado@gmail.com>
2023-06-20 11:40:18 -0700

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""task 4"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+    def area(self):
+        """area is not implemented exception"""
+        raise Exception("area() is not implemented")
