a3964096e7e2b32d29149b7eb3b454c55294b6d9
kundi21 <cundi.machado@gmail.com>
2023-06-20 12:22:55 -0700

    8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,9 +2,9 @@
 """task 8"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Rectangle Class with inherits from BaseException"""
-        
     def __init__(self, width, height):
         """width and height"""
         self.integer_validator("width", width)
