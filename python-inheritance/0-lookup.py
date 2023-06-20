5aa2b5ea33f23c596e53dfbe86c259ad9c963c56
kundi21 <cundi.machado@gmail.com>
2023-06-20 10:37:25 -0700

    0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,6 +3,5 @@
 
 
 def lookup(obj):
-    """returns the list of available attributes 
-    and methods of an object """
+    """returns the list of available attributes and methods of an object """
     return list(dir(obj))
