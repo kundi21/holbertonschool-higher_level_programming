2ad595d3072df9c9c6236343dbff6e39c95dae75
kundi21 <cundi.machado@gmail.com>
2023-06-20 12:57:51 -0700

    10-square.py

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""task 8"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square Class"""
+    def __init__(self, size):
+        """initializator"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size * self.__size
