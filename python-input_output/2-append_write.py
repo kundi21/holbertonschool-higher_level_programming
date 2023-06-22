ad51a9694ea5db850f42196203fac361d35286cd
kundi21 <cundi.machado@gmail.com>
2023-06-22 14:42:17 -0700

    2-append_write.py

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""task 1"""
+
+
+def append_write(filename="", text=""):
+    """function that appends a string at the end of a
+    text file (UTF8) and returns the number of characters added"""
+    with open(filename, mode="a", encoding="utf-8") as myFile:
+        myFile.write(text)
+        return len(text)
