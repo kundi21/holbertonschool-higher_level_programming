78eb5268b875373b6824c87da94171db9c84d7ff
kundi21 <cundi.machado@gmail.com>
2023-06-22 14:40:09 -0700

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,6 +5,6 @@
 def write_file(filename="", text=""):
     """writes a string to a text file (UTF8)
     and returns the number of characters written"""
-    with open(filename, mode= "w", encoding="utf-8") as myFile:
+    with open(filename, mode="w", encoding="utf-8") as myFile:
         myFile.write(text)
         return len(text)
