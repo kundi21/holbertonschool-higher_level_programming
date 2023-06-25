78f0ae6fcb30d7ef7e21ed47793f1675c035c660
kundi21 <cundi.machado@gmail.com>
2023-06-25 12:12:39 -0700

    4-from_json_string.py

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -3,5 +3,6 @@ import json
 
 
 def from_json_string(my_str):
-    """ returns the JSON representation of an object (string)"""
+    """function that returns an object (Python data structure)
+    represented by a JSON string"""
     return json.loads(my_str)
