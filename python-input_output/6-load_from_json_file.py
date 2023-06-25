b3ec9126858def555646df0453e2f20efb6db54a
kundi21 <cundi.machado@gmail.com>
2023-06-25 13:32:17 -0700

    6-load_from_json_file.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-import json
 """task 6"""
+
+import json
 
 
 def load_from_json_file(filename):
-    """unction that creates an Object from a “JSON file”"""
-    with open(filename, mode="r", encoding="utf-8") as f:
-        return json.load(f)
+    """ function that creates an Object from a “JSON file”"""
+    with open(filename, mode='r', encoding="utf-8") as file:
+        json = file.read()
+        obj = json.loads(json)
+        return obj
+
